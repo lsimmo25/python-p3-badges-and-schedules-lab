31661d1112d0d4207b8cf9b323fc5c81b8cf479f
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-17 12:31:01 -0400

    lab complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,31 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = badge_maker(name)
+        badges.append(badge)
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    room_number = 1
+    for name in names:
+        if room_number <= 8:
+            message = f"Hello, {name}! You'll be assigned to room {room_number}!"
+            room_assignments.append(message)
+            room_number += 1
+        else:
+            break
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+    for room in room_assignments:
+        print(room)
+    
